914a5e61a12244d7da9ae09f3590bf1334c5377a
kerruba-milkman <luca.cherubin@milkmantechnologies.com>
2022-05-24 17:59:58 +0200

    Make script name more talkative

--- .ci/verify_version_deployability.sh
+++ .ci/verify_version_deployability.sh
@@ -5,10 +5,12 @@ VERSION=$(jq -r .version package.json)
 ALL_VERSIONS=$(aws codeartifact list-package-versions --package milkman-api-js-client --namespace milkman --domain milkmantechnologies --repository milkman --format npm --query 'versions[*].[version]' --output text)
 PRESENT=$(echo "$ALL_VERSIONS" | sed -n "/$VERSION/p") 
 
-if [[ -n "$ALREADY_DEPLOYED" ]]; then
+if [[ -n "$PRESENT" ]]; then
+  echo
   echo "ERROR: Version $VERSION already deployed on CodeArtifact! Please update the version of your package"
+  echo
   exit 1
 fi
 
-echo "Version is alright"
+echo "Version $VERSION is valid to be deployed on CodeArtifact"
 exit 0
